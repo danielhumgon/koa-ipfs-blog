39312626ef1454be8e21efa0f9ca842eecb6ec55
Daniel Gonzalez <danielhumgon@gmail.com>
2019-05-30 23:09:53 -0400

    add bch

---
+++ config/env/common.js
@@ -0,0 +1,9 @@
+/*
+  This file is used to store unsecure, application-specific data common to all
+  environments.
+*/
+
+module.exports = {
+  port: process.env.PORT || 3210,
+  BCHADDR: `bitcoincash:qzx5w7zqm5geykxaydvfpctxf9qklv4ylg7wwxckkg`
+}
