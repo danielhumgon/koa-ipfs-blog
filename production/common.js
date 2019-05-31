e845290d80d1f1e83431db5ad998e079c6c97332
Daniel Gonzalez <danielhumgon@gmail.com>
2019-05-30 23:11:10 -0400

    delete common

--- production/common.js
+++
@@ -1,9 +0,0 @@
-/*
-  This file is used to store unsecure, application-specific data common to all
-  environments.
-*/
-
-module.exports = {
-  port: process.env.PORT || 3210,
-  BCHADDR: `bitcoincash:qppngav5s88devt4ypv3vhgj643q06tctcx8fnzewp`
-}
